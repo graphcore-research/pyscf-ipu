251e563fc3e9c161adb0ae6bf4f89dcb362a29f6
alexandermath <alexm@graphcore.ai>
2023-07-04 13:08:42 +0000

    Adding nanoDFT back.

--- nanoDFT.py
+++ nanoDFT.py
@@ -1,3 +1,7 @@
+# barebone DFT implementation
+# several sacrifices
+# - f32 
+# - add autograd? 
 import jax
 import jax.numpy as jnp
 import numpy as np
@@ -7,7 +11,8 @@ from electron_repulsion.direct import prepare_integrals_2_inputs, compute_integr
 jax.config.FLAGS.jax_platform_name = 'cpu'
 hartree_to_eV    = 27.2114079527 
 
-def nanoGPT_iteration(i, vals):
+def nanoDFT_iteration(i, vals):
+    # 
     mask, V_xc, density_matrix, _V, _H, mf_diis_H, vj, vk, overlap, electron_repulsion, \
         fixed_hamiltonian, L_inv, weights, hyb, ao, num_calls, iter_matrices, part_energies = vals
     N = density_matrix.shape[0]
@@ -23,15 +28,18 @@ def nanoGPT_iteration(i, vals):
     # Step 3: Use result from eigenproblem to build new density matrix.
     eigvects           = eigvects * mask.reshape(1, -1) # the same as "eigvects[:, :n_electrons_half]"
     density_matrix     = 2 * eigvects @ eigvects.T
-    E_xc, V_xc, vj, vk = xc( density_matrix, ao, electron_repulsion, weights, vj, vk, hyb, num_calls)
+    E_xc, V_xc, vj, vk = exchange_correlation( density_matrix, ao, electron_repulsion, weights, vj, vk, hyb, num_calls)
 
-    # Tensors used to compute energy/hlgap on CPU in float64. 
+    # Compute energy here? 
     part_energies = part_energies.at[i].set(  E_xc )
     iter_matrices = jax.lax.dynamic_update_slice(iter_matrices, jnp.stack((density_matrix, vj, vk, hamiltonian)).reshape(1, 4, N, N), (i, 0, 0, 0))
 
     return [mask, V_xc, density_matrix, _V, _H, mf_diis_H, vj, vk, overlap, electron_repulsion, fixed_hamiltonian, L_inv, weights, hyb, ao, num_calls, iter_matrices, part_energies]
 
-def xc(density_matrix, ao, electron_repulsion, weights, vj, vk, hyb, num_calls):
+def exchange_correlation(density_matrix, ao, electron_repulsion, weights, vj, vk, hyb, num_calls):
+    # 
+    # 
+    #
     assert args.xc == "b3lyp"
     n = density_matrix.shape[0]
 
@@ -46,8 +54,8 @@ def xc(density_matrix, ao, electron_repulsion, weights, vj, vk, hyb, num_calls):
 
     n, p = weird_rho.shape
     V_xc = jnp.sum( (ao * weird_rho.reshape(n, p, 1)), axis=0)
-    V_xc      = ao[0].T @ V_xc
-    V_xc      = V_xc + V_xc.T
+    V_xc = ao[0].T @ V_xc
+    V_xc = V_xc + V_xc.T
 
     d = num_calls.size
 
@@ -56,24 +64,22 @@ def xc(density_matrix, ao, electron_repulsion, weights, vj, vk, hyb, num_calls):
         vj, vk = jax.jit(ipu_direct_mult, backend="ipu", static_argnums=(2,3,4,5,6,7,8,9))( electron_repulsion, density_matrix, _tuple_indices, _tuple_do_lists, _N, num_calls.size, tuple(args.indxs.tolist()), tuple(args.indxs.tolist()), int(args.threads), v=int(args.multv)) 
         vk = vk*hyb
     else:
-        d = density_matrix.shape[0]
-        E = electron_repulsion
-        vj = jnp.sum(E.reshape(d**2, d**2) * density_matrix.reshape(1, -1), axis=1).reshape(d,d)
-        vk = jnp.sum(E.transpose(1,2,0,3).reshape(d**2, d**2) * density_matrix.reshape(1, -1), axis=1).reshape(d,d)*jnp.asarray(hyb, dtype=E.dtype)
+        vj = jnp.einsum('ijkl,ji->kl', electron_repulsion, density_matrix)
+        vk = jnp.einsum('ijkl,jk->il', electron_repulsion, density_matrix) * jnp.asarray(hyb , dtype=electron_repulsion.dtype) 
 
     V_xc    = V_xc + vj - vk/2
 
     return E_xc, V_xc, vj, vk
 
-
 def DIIS(i, sdf, hamiltonian, _V, _H, mf_diis_H):
-    # Update hamiltonian as linear combination of previous iterations
-    mf_diis_head      = i % _V.shape[0]
-    nd, d             = _V.shape
-
-    errvec = (sdf - sdf.T)
+    # DIIS is an optional technique to improve DFT convergence. DIIS computes 
+    #   hamiltonian_i = c_1 hamiltonian_{i-1} + ... + c_8 hamiltonian_{i-8}  where  c=pinv(some_matrix)[0,:]
+    # I thus like to think of DIIS as "fancy momentum". 
+    mf_diis_head = i % _V.shape[0]
+    nd, d        = _V.shape
 
     # Store current (hamiltonian,errvec) as flattened as row inside _V and _H.
+    errvec = (sdf - sdf.T)
     _V = jax.lax.dynamic_update_slice(_V, errvec.reshape(1, d),      (mf_diis_head, 0))
     _H = jax.lax.dynamic_update_slice(_H, hamiltonian.reshape(1, d), (mf_diis_head, 0))
 
@@ -107,9 +113,9 @@ def DIIS(i, sdf, hamiltonian, _V, _H, mf_diis_H):
     return hamiltonian, _V, _H, mf_diis_H 
 
 
-def _nanoGPT(density_matrix, kinetic, nuclear, overlap, ao, 
+def _nanoDFT(density_matrix, kinetic, nuclear, overlap, ao, 
                 electron_repulsion, weights, 
-                mf_diis_space, N, hyb, mask, _input_floats, _input_ints, L_inv=None):
+                mf_diis_space, N, hyb, mask, _input_floats, _input_ints, L_inv):
     # --- INITIALIZE MATRICES USED FOR DIIS --- #
     mf_diis_H       = np.zeros((mf_diis_space+1, mf_diis_space+1))
     mf_diis_H[0,1:] = mf_diis_H[1:,0] = 1
@@ -136,9 +142,9 @@ def _nanoGPT(density_matrix, kinetic, nuclear, overlap, ao,
         num_calls = electron_repulsion.shape[0]
 
     _num_calls = np.zeros(num_calls)
-    _, V_xc, vj, vk = xc( density_matrix, ao, electron_repulsion, weights, vj, vk, hyb, _num_calls)
+    _, V_xc, vj, vk = exchange_correlation( density_matrix, ao, electron_repulsion, weights, vj, vk, hyb, _num_calls)
 
-    vals = jax.lax.fori_loop(0, args.its, nanoGPT_iteration, [mask, V_xc, density_matrix, _V, _H, mf_diis_H, vj, vk, overlap, electron_repulsion,  
+    vals = jax.lax.fori_loop(0, args.its, nanoDFT_iteration, [mask, V_xc, density_matrix, _V, _H, mf_diis_H, vj, vk, overlap, electron_repulsion,  
                                                              fixed_hamiltonian, L_inv, weights, hyb, ao, _num_calls, iter_matrices, part_energies])
     iter_matrices = vals[-2]
     part_energies = vals[-1]
@@ -146,8 +152,13 @@ def _nanoGPT(density_matrix, kinetic, nuclear, overlap, ao,
     return iter_matrices, fixed_hamiltonian, part_energies, L_inv 
 
 def nanoDFT(str, mf_diis_space=9):
+    # CPU/pyscf_init
+    # jax_init
+    # jax_forloop
+    # jax_finalize 
+
     mol = pyscf.gto.mole.Mole()
-    mol.build(atom=str, unit="Angstrom", basis=args.basis, spin=args.spin, verbose=0)
+    mol.build(atom=str, unit="Angstrom", basis=args.basis, spin=0, verbose=0)
 
     n_electrons       = mol.nelectron
     n_electrons_half  = n_electrons//2
@@ -172,6 +183,7 @@ def nanoDFT(str, mf_diis_space=9):
     overlap         = mol.intor_symmetric('int1e_ovlp').reshape(N, N)
 
     if args.backend == "cpu" or args.ipumult: electron_repulsion = mol.intor("int2e_sph")
+    else: electron_repulsion = None 
 
     # Turns generalized eigenproblem into eigenproblem.
     fixed_hamiltonian   = kinetic + nuclear
@@ -179,16 +191,25 @@ def nanoDFT(str, mf_diis_space=9):
 
     input_floats, input_ints = prepare_integrals_2_inputs(mol)[:2]
 
-    vals = jax.jit(_nanoGPT, static_argnums=(7,8))( density_matrix, kinetic, nuclear, overlap, ao, electron_repulsion, weights, 
+    vals = jax.jit(_nanoDFT, static_argnums=(7,8), backend=args.backend)( density_matrix, kinetic, nuclear, overlap, ao, electron_repulsion, weights, 
                                                     mf_diis_space, N, hyb , mask, input_floats, input_ints, L_inv)
 
-    iter_matrices, fixed_hamiltonian, part_energies, _  = [np.asarray(a).astype(np.float64) for a in vals]
+
+    # compute both in f32/f64 and compare. 
+    # this will allow us compare the energy computation errors? 
+    # also, compare the different terms?
+
+    iter_matrices, fixed_hamiltonian, part_energies, _  = [np.asarray(a).astype(np.float32) for a in vals]
     density_matrices, vjs, vks, hamiltonians            = [iter_matrices[:, i] for i in range(4)]
 
     mo_occ = np.concatenate([np.ones(n_electrons_half)*2, np.zeros(N-n_electrons_half)])
     lumo   = np.argmin(np.array(mo_occ))
     homo   = lumo - 1
     d      = L_inv.shape[0]
+
+    # 
+    def energy(density_matrix, vk, vj, fixed_hamiltonian, linalg_backendnp):
+        return np.sum(fixed_hamiltonian * density_matrices[i]) + E_coulomb + nuclear_energy + E_xc
 
     energies = np.zeros(args.its)
     hlgaps   = np.zeros(args.its)
@@ -198,6 +219,7 @@ def nanoDFT(str, mf_diis_space=9):
         E_xc        = part_energies[i] - np.sum(density_matrix * vks[i]) * 0.25
         energies[i] = np.sum(fixed_hamiltonian * density_matrices[i]) + E_coulomb + nuclear_energy + E_xc
 
+
         mo_energy   = np.linalg.eigh(L_inv @ hamiltonians[-1].reshape(d, d) @ L_inv.T)[0]
         hlgaps[i]   = np.abs( mo_energy[homo] - mo_energy[lumo] )
 
@@ -206,7 +228,7 @@ def nanoDFT(str, mf_diis_space=9):
 
 def _eigh(x):
     if args.backend == "ipu":
-        from jax_ipu_research.tile import ipu_eigh
+        from jax_ipu_experimental_addons.tile import ipu_eigh
         n = x.shape[0]
         pad = n % 2
         print(x.dtype)
@@ -236,52 +258,31 @@ def pinv0(a):  # take out first row
 if __name__ == "__main__":  
     import argparse
     parser = argparse.ArgumentParser(description='Arguments for Density Functional Theory. ')
-    parser.add_argument('-skipdiis',  action="store_true", help='Whether to skip DIIS; useful for benchmarking.')
-    parser.add_argument('-num',       default=10,          type=int,   help='Run the first "num" test molecules. ')
-    parser.add_argument('-id',        default=126,          type=int,   help='Run only test molecule "id". ')
-    parser.add_argument('-its',       default=20,          type=int,   help='Number of Kohn-Sham iterations. ')
-    parser.add_argument('-step',      default=1,           type=int,   help='If running 1000s of test cases, do molecules[args.skip::args.step]]')
-    parser.add_argument('-spin',      default=0,           type=int,   help='Even or odd number of electrons? Currently only support spin=0')
-    parser.add_argument('-str',       default="",          help='Molecule string, e.g., "H 0 0 0; H 0 0 1; O 1 0 0; "')
-    parser.add_argument('-numerror', action="store_true",     help='Save all tensors to debug numerical errors. ')
-    parser.add_argument('-ipumult', action="store_true",     help='On IPU do mult using full tensor ERI computed using PySCF (and not our Rys Quadrature implementation). ')
-    parser.add_argument('-float32',   action="store_true", help='Whether to use float32 (default is float64). ')
-    parser.add_argument('-basis',     default="STO-3G",    help='Which basis set to use. ')
-    parser.add_argument('-xc',        default="b3lyp",     help='Only support B3LYP. ')
-    parser.add_argument('-backend',   default="cpu",       help='Which backend to use, e.g., -backend cpu or -backend ipu')
+    parser.add_argument('-its',       default=20,            type=int,   help='Number of Kohn-Sham iterations. ')
+    parser.add_argument('-str',       default="",            help='Molecule string, e.g., "H 0 0 0; H 0 0 1; O 1 0 0; "')
+    parser.add_argument('-ipumult',   action="store_true",   help='For IPU. Use (N, N, N, N) ERI computed with PySCF on CPU. (and not our Rys Quadrature implementation). ')
+    parser.add_argument('-float32',   action="store_true",   help='Whether to use float32 (default is float64). ')
+    parser.add_argument('-basis',     default="STO-3G",      help='Which basis set to use. ')
+    parser.add_argument('-xc',        default="b3lyp",       help='Only support B3LYP. ')
+    parser.add_argument('-backend',   default="cpu",         help='Which backend to use: "-backend cpu" or "-backend ipu".')
+    parser.add_argument('-level',     default=2,             help="Level of grids for XC numerical integration (default=2). ", type=int)
+    parser.add_argument('-gdb',       default=-1, type=int,  help='Which version of GDP to load {10, 11, 13, 17}. ')
+    parser.add_argument('-multv',     default=2,  type=int,  help='Which version of our einsum algorithm to use;comptues ERI@flat(v). Different versions trades-off for memory vs sequentiality. ')
+    parser.add_argument('-intv',      default=1,  type=int,  help='Which version to use of our integral algorithm. ')
+    parser.add_argument('-skipdiis',  action="store_true",   help='Whether to skip DIIS; useful for benchmarking.')
 
-    parser.add_argument('-level',    default=2, help="Level of the grids used by us (default=2). ", type=int)
-    parser.add_argument('-plevel',   default=2, help="Level of the grids used by pyscf (default=2). ", type=int)
-    parser.add_argument('-gdb',        default=-1, type=int,  help='Which version of GDP to load {10, 11, 13, 17}. ')
-    parser.add_argument('-multv',    default=2, type=int, help='Which version of our einsum algorithm to use;comptues ERI@flat(v). Different versions trades-off for memory vs sequentiality. ')
-    parser.add_argument('-intv',    default=1, type=int, help='Which version to use of our integral algorithm. ')
-
-    parser.add_argument('-jit',  action="store_true")
-    parser.add_argument('-profile',  action="store_true", help="Stops script in generation mode after one molecule; useful when using popvision to profile for -backend ipu")
-    parser.add_argument('-pyscf',  action="store_true", help="Used to compute with reference implementation. ")
-    parser.add_argument('-uniform_pyscf',  default = -1, type=float, help="Use reference implementation PySCF if 'np.random.uniform(0,1,(1))<args.uniform_pyscf'")
-    parser.add_argument('-threads',  default=1, type=int, help="Number of threads to use to compute ipu_mult_direct. ")
-    parser.add_argument('-threads_int',  default=1, type=int, help="Number of threads to use to do int2e_sph, accepts {1,...,6}. ")
-    parser.add_argument('-split',  default=[1, 16], type=int, nargs="+", help='How to split during data generation over multiple POD16s. 7 47 means split dataset into 47 chunks and this IPU handles chunk 7 (zero indexed).')
-    parser.add_argument('-limit', default=-1, type=int, help='smiles = args.smiles[:limit]; gdb molecules are sorted by hydrogens, this allows us to take the ones with fewer hydrogens for which DFT is faster. ')
-    parser.add_argument('-gname',  default="", type=str, help='Folder name to store generate dataset; useful when using multiple pods to generate. ')
-    parser.add_argument('-geneigh',  action="store_true" , help='Use generalized eigendecomposition like pyscf; relies on scipy, only works in debug mode with -forloop. ')
+    parser.add_argument('-threads',      default=1, type=int, help="For -backend ipu. Number of threads for einsum(ERI, dm) with custom C++ (trades-off speed vs memory).  ")
+    parser.add_argument('-threads_int',  default=1, type=int, help="For -backend ipu. Number of threads for computing ERI with custom C++ (trades off speed vs memory). ")
 
     args = parser.parse_args()
 
     import sys
-    print(sys.argv)
     from natsort import natsorted
     print(natsorted(vars(args).items()) )
 
     print("[BASIS]", args.basis)
 
     sys.argv = sys.argv[:1]
-
-    if args.numerror:
-        args.forloop = True
-        args.backend = "cpu"
-        args.its = 35
 
     print("")
 
@@ -314,13 +315,11 @@ if __name__ == "__main__":
         if args.gdb == 2:  args.smiles = ['CC']*1000
 
         print("Length GDB: ", len(args.smiles))
-
         from rdkit import Chem 
         from rdkit.Chem import AllChem
         from rdkit import RDLogger
         lg = RDLogger.logger()
         lg.setLevel(RDLogger.CRITICAL)
-
 
         for i in range(0, len(args.smiles)): 
             smile = args.smiles[i]
@@ -345,15 +344,14 @@ if __name__ == "__main__":
 
             atom_string, string = get_atom_string(" ".join(atoms), locs)
 
-            print(string)
             break
 
         args.str = string 
 
-    # Test Case: Compare nanoGPT against PySCF. 
+    # Test Case: Compare nanoDFT against PySCF. 
     molecule_string = args.str
     mol = pyscf.gto.mole.Mole()
-    mol.build(atom=molecule_string, unit="Angstrom", basis=args.basis, spin=args.spin, verbose=0)
+    mol.build(atom=molecule_string, unit="Angstrom", basis=args.basis, spin=0, verbose=0)
 
     print("\t", molecule_string, end="")
 
@@ -361,8 +359,8 @@ if __name__ == "__main__":
 
     mol = pyscf.gto.mole.Mole()
     mol.verbose = 0
-    pyscf.__config__.dft_rks_RKS_grids_level = args.plevel
-    mol.build(atom=molecule_string, unit='Angstrom', basis=args.basis, spin=args.spin)
+    pyscf.__config__.dft_rks_RKS_grids_level = args.level
+    mol.build(atom=molecule_string, unit='Angstrom', basis=args.basis, spin=0)
 
     mol.max_cycle = args.its
     mf = pyscf.scf.RKS(mol)
@@ -390,6 +388,3 @@ if __name__ == "__main__":
     print("chemAcc: \t%15f"%0.043)
     print("chemAcc/diff: \t%15f"%(0.043/np.abs(pyscf_energy-energies[-1])))
     print("chemAcc/mdiff: \t%15f"%(0.043/np.abs(pyscf_energy-np.mean(energies[-10:]))))
-    print("> diffs:")
-    print(np.abs(energies.reshape(-1)[-5:] - pyscf_energy))
-    print("mean+-var: %f +- %f"%( np.mean(np.abs(energies.reshape(-1)[-5:] - pyscf_energy)), np.var(np.abs(energies.reshape(-1)[-5:] - pyscf_energy))))
