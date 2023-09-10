2c6747278b40e0ff160f60207a64ee6236c2c315
alexandermath <alexm@graphcore.ai>
2023-09-10 18:26:54 +0000

    CPU version using int32 through (1) difference compression with jnp.diff/jnp.cumsum and (2) simulated split(int64)=(int32, int32) to deal with offsets in int64 and cumsum in int64 while using int32. Also includes C++ code which uses int64_t which ?gets compiled to simulated int64? CPU code works for edge-case test where int32 alone breaks (-edge); IPU code breaks, likely due to simple reason.

--- nanoDFT/compute_indices.cpp
+++ nanoDFT/compute_indices.cpp
@@ -42,7 +42,7 @@ ij_pair get_i_j(int32_t val) {
 
 class SymmetryIndices : public Vertex {
 public:
-  InOut<Vector<int>>  value;
+  Input<Vector<int>>  value;
   Input<Vector<int>>  symmetry;
   Input<Vector<int>>  input_N;
 
@@ -66,6 +66,109 @@ public:
       int j = pair_ij.j;
       int k = pair_kl.i;
       int l = pair_kl.j;
+
+      switch (symmetry.data()[0]) {
+        //dm_indices_func_J = lambda i,j,k,l,symmetry: jnp.array([i*N+j, j*N+i, i*N+j, j*N+i, k*N+l, l*N+k, k*N+l, l*N+k])[symmetry]
+        //i*N+j, j*N+i, i*N+j, j*N+i, k*N+l, l*N+k, k*N+l, l*N+k
+        case 0: { out[iteration] = i*N+j; break; }
+        case 1: { out[iteration] = j*N+i; break; }
+        case 2: { out[iteration] = i*N+j; break; }
+        case 3: { out[iteration] = j*N+i; break; }
+        case 4: { out[iteration] = k*N+l; break; }
+        case 5: { out[iteration] = l*N+k; break; }
+        case 6: { out[iteration] = k*N+l; break; }
+        case 7: { out[iteration] = l*N+k; break; }
+
+        //ss_indices_func_J = lambda i,j,k,l,symmetry: jnp.array([k*N+l, k*N+l, l*N+k, l*N+k, i*N+j, i*N+j, j*N+i, j*N+i])[symmetry]
+        case 8: { out[iteration]  = k*N+l; break; }
+        case 9: { out[iteration]  = k*N+l; break; }
+        case 10: { out[iteration] = l*N+k; break; }
+        case 11: { out[iteration] = l*N+k; break; }
+        case 12: { out[iteration] = i*N+j; break; }
+        case 13: { out[iteration] = i*N+j; break; }
+        case 14: { out[iteration] = j*N+i; break; }
+        case 15: { out[iteration] = j*N+i; break; }
+
+        //dm_indices_func_K = lambda i,j,k,l,symmetry: jnp.array([k*N+j, k*N+i, l*N+j, l*N+i, i*N+l, i*N+k, j*N+l, j*N+k])[symmetry]
+        case 16: { out[iteration] = k*N+j; break; }
+        case 17: { out[iteration] = k*N+i; break; }
+        case 18: { out[iteration] = l*N+j; break; }
+        case 19: { out[iteration] = l*N+i; break; }
+        case 20: { out[iteration] = i*N+l; break; }
+        case 21: { out[iteration] = i*N+k; break; }
+        case 22: { out[iteration] = j*N+l; break; }
+        case 23: { out[iteration] = j*N+k; break; }
+
+        //ss_indices_func_K = lambda i,j,k,l,symmetry: jnp.array([i*N+l, j*N+l, i*N+k, j*N+k, k*N+j, l*N+j, k*N+i, l*N+i])[symmetry]
+        case 24: { out[iteration] = i*N+l; break; }
+        case 25: { out[iteration] = j*N+l; break; }
+        case 26: { out[iteration] = i*N+k; break; }
+        case 27: { out[iteration] = j*N+k; break; }
+        case 28: { out[iteration] = k*N+j; break; }
+        case 29: { out[iteration] = l*N+j; break; }
+        case 30: { out[iteration] = k*N+i; break; }
+        case 31: { out[iteration] = l*N+i; break; }
+      }
+    }
+    return true ;
+  }
+
+};
+
+
+
+int64_t max_val_int64(int64_t N) {
+    //double x_candidate_f = (-1 + sqrt(1 + 8 * (double)N)) / 2; 
+    int64_t x_candidate_f = (-1 + sqrt(1 + 8 * (int64_t)N)) / 2; 
+    int64_t x_candidate = (int64_t) floor(x_candidate_f);
+    int64_t x = ((x_candidate * (x_candidate + 1)) / 2 <= N) ? x_candidate : x_candidate - 1;
+    return x;
+}
+
+typedef struct {
+    int64_t i;
+    int64_t j;
+} ij_pair_int64;
+
+
+ij_pair_int64 get_i_j_int64(int64_t val) {
+    ij_pair_int64 result;
+    result.i = max_val_int64(val);
+    result.j = val - result.i * (result.i + 1) / 2;
+    return result;
+}
+
+
+class PairSymmetryIndices : public Vertex {
+public:
+  Input<Vector<int>> value_low;
+  Input<Vector<int>> value_high;
+  Input<Vector<int>> symmetry;
+  Input<Vector<int>> input_N;
+
+  Input<Vector<int>> start;
+  Input<Vector<int>> stop; 
+
+  Output<Vector<int>> out; 
+
+  bool compute() {
+    int N = input_N.data()[0];
+
+    for (unsigned int iteration = start.data()[0]; iteration < stop.data()[0]; iteration++){
+    //for (unsigned int iteration = 0; iteration < out.size(); iteration++){
+      int64_t current_val = (int64_t)value_low.data()[iteration] + 
+                            //(((int64_t)value_high.data()[iteration]) << 31);
+                            (int64_t)(value_high.data()[iteration] << 31);
+
+      int64_t ij = max_val_int64(current_val);
+      int64_t kl = current_val - ij*(ij+1)/2;
+
+      ij_pair_int64 pair_ij = get_i_j_int64(ij);
+      ij_pair_int64 pair_kl = get_i_j_int64(kl);
+      int i = (int)pair_ij.i;
+      int j = (int)pair_ij.j;
+      int k = (int)pair_kl.i;
+      int l = (int)pair_kl.j;
 
       switch (symmetry.data()[0]) {
         //dm_indices_func_J = lambda i,j,k,l,symmetry: jnp.array([i*N+j, j*N+i, i*N+j, j*N+i, k*N+l, l*N+k, k*N+l, l*N+k])[symmetry]
--- nanoDFT/sparse_symmetric_ERI.py
+++ nanoDFT/sparse_symmetric_ERI.py
@@ -7,12 +7,34 @@ import os.path as osp
 from tessellate_ipu import create_ipu_tile_primitive, ipu_cycle_count, tile_map, tile_put_sharded, tile_put_replicated
 from functools import partial 
 jax.config.update('jax_platform_name', "cpu")
-jax.config.update('jax_enable_x64', False) 
+jax.config.update("jax_debug_nans", True)
 HYB_B3LYP = 0.2
 
+import numpy as np 
+import jax 
+import jax.numpy as jnp 
+
+def matmul_cumsum_jax(arr):
+    return jnp.tril(jnp.ones((len(arr), len(arr)))) @ arr 
+
+def cumsum_jax(arr):
+    chunk_size = 2**7 
+    original_shape = arr.shape 
+    padding = chunk_size - (len(arr) % chunk_size) if len(arr) % chunk_size != 0 else 0
+    arr = jnp.pad(arr, (0, padding))  
+    num_chunks = -(-len(arr) // chunk_size) 
+    chunks = arr.T.reshape(num_chunks, chunk_size) 
+    chunks = jax.vmap(matmul_cumsum_jax)(chunks)
+    offset = 0
+    offsets = [offset]
+    for i, chunk in enumerate(chunks):
+        offset += chunk[-1]
+        offsets.append(offset)
+    chunks = jax.vmap(jax.lax.add, in_axes=(0,0))(chunks.astype(jnp.int32), jnp.array(offsets[:-1], dtype=np.int32))
+    return jnp.concatenate(chunks).reshape(-1)[:original_shape[0]]
+
 def max_val(N):
-    x_candidate = (-1 + jnp.sqrt(1 + 8*N)) // 2 # TODO: Check when this overflows and fix problem (e.g. N=500 => N^4/8 =7.8G > 2**32 for np.uint32).
-    x_candidate = (x_candidate).astype(jnp.int32) 
+    x_candidate = (-1 + jnp.sqrt(1 + 8*N.astype(np.int64))) // 2 
     x = jnp.where(x_candidate * (x_candidate + 1) // 2 <= N, x_candidate, x_candidate - 1)
     return x
 max_vals  = jax.vmap(max_val)
@@ -27,37 +49,45 @@ def cpu_ijkl(value, symmetry, f):
     i, j = get_i_j(ij)
     k, l = get_i_j(kl)
     v      = f(i,j,k,l,symmetry) 
-    return v
-cpu_ijkl = jax.vmap(cpu_ijkl, in_axes=(0, None, None, None))
+    return v.astype(np.int64), (i,j,k,l)
+cpu_ijkl = jax.vmap(cpu_ijkl, in_axes=(0, None, None))
+
 
 @partial(jax.jit, backend="ipu")
 def ipu_ijkl(nonzero_indices, symmetry, N):
     vertex_filename  = osp.join(osp.dirname(__file__), "compute_indices.cpp")
     compute_indices= create_ipu_tile_primitive(
-            "SymmetryIndices",
-            "SymmetryIndices",
-            inputs=["value", "symmetry", "input_N", "start", "stop"],
+            "PairSymmetryIndices",
+            "PairSymmetryIndices",
+            inputs=["value_low", "value_high", "symmetry", "input_N", "start", "stop"],
             outputs={"out": 0},
             gp_filename=vertex_filename,
             perf_estimate=100,
     )
+    nonzero_indices_low = nonzero_indices[0]
+    nonzero_indices_high = nonzero_indices[1]
 
-    size = np.prod(nonzero_indices.shape)
+    size = np.prod(nonzero_indices_low.shape)
     total_threads = (1472-1) * 6 
     remainder = size % total_threads
     if remainder != 0: 
-        nonzero_indices = jnp.pad(nonzero_indices, (0, total_threads-remainder))
-    nonzero_indices = nonzero_indices.reshape(total_threads, -1) 
+        nonzero_indices_low = jnp.pad(nonzero_indices_low, (0, total_threads-remainder))
+        nonzero_indices_high = jnp.pad(nonzero_indices_high, (0, total_threads-remainder))
+    nonzero_indices_low = nonzero_indices_low.reshape(total_threads, -1) 
+    nonzero_indices_high = nonzero_indices_high.reshape(total_threads, -1) 
 
     tiles = tuple((np.arange(0,total_threads) % (1471) + 1).astype(np.int32).tolist())
-    nonzero_indices = tile_put_sharded(nonzero_indices, tiles)
+    nonzero_indices_high = tile_put_sharded(nonzero_indices_high, tiles)
+    nonzero_indices_low = tile_put_sharded(nonzero_indices_low, tiles)
     symmetry = jnp.array(symmetry, dtype=jnp.int32).reshape(1,)
     symmetry = tile_put_replicated(symmetry,   tiles) 
     N        = tile_put_replicated(N,   tiles)
     start    = tile_put_replicated(0,   tiles)
-    stop     = tile_put_replicated(nonzero_indices.shape[1],   tiles)
+    stop     = tile_put_replicated(nonzero_indices_low.shape[1],   tiles)
 
-    value = tile_map(compute_indices, nonzero_indices, symmetry, N, start, stop)  
+    print(nonzero_indices_low.dtype, nonzero_indices_high.dtype, symmetry.dtype, N.dtype, start.dtype,stop.dtype)
+
+    value = tile_map(compute_indices, nonzero_indices_low, nonzero_indices_high, symmetry, N, start, stop)  
 
     return value.array.reshape(-1)[:size]
 
@@ -78,10 +108,15 @@ def num_repetitions_fast(value):
     return repetitions
 vmap_num_repetitions_fast = jax.vmap(num_repetitions_fast, in_axes=(0))
 
-def sparse_symmetric_einsum(nonzero_distinct_ERI, nonzero_indices, dm, backend):
+def sparse_symmetric_einsum(nonzero_distinct_ERI, nonzero_indices, offset, dm, backend, nipu, pad_length):
     dm = dm.reshape(-1)
     diff_JK = jnp.zeros(dm.shape)
     N = int(np.sqrt(dm.shape[0]))
+
+    assert nonzero_indices.dtype == np.int32, nonzero_indices.dtype
+    assert offset[0].dtype == np.int32, offset[0].dtype
+    assert offset[1].dtype == np.int32, offset[1].dtype
+    #nonzero_indices = jnp.cumsum(nonzero_indices, axis=1) 
     
     if backend == "cpu":
         indices_func = lambda i,j,k,l,symmetry: jnp.array([i*N+j, j*N+i, i*N+j, j*N+i, k*N+l, l*N+k, k*N+l, l*N+k,
@@ -94,31 +129,66 @@ def sparse_symmetric_einsum(nonzero_distinct_ERI, nonzero_indices, dm, backend):
         is_K_matrix = (symmetry >= 8)
 
         def sequentialized_iter(i, vals):
-            # Generalized J/K computation: does J when symmetry is in range(0,8) and K when symmetry is in range(8,16)
-            # Trade-off: Using one function leads to smaller always-live memory.
             diff_JK = vals 
-
-            indices = nonzero_indices[i]
             eris    = nonzero_distinct_ERI[i]
 
-            if backend == "cpu": dm_indices = cpu_ijkl(indices, symmetry+is_K_matrix*8, indices_func)  
-            else:                dm_indices = ipu_ijkl(indices, symmetry+is_K_matrix*8, N)  
-            dm_values  = jnp.take(dm, dm_indices, axis=0) # causes peak 1 
-            print(eris.shape, dm_values.shape, indices.shape)
+            def add_int64(a, b):
+                # Unpack (low, high) pairs
+                low_a, high_a = a
+                low_b, high_b = b
+                
+                # Perform the addition on the low 32 bits
+                low_sum = jnp.int32(low_a) + jnp.int32(low_b)
+                
+                # Calculate the carry: 1 if overflow occurred, otherwise 0
+                carry = jnp.int32((low_sum < low_a) | (low_sum < low_b))
+                
+                # Add the high 32 bits, along with the carry
+                high_sum = jnp.int32(high_a) + jnp.int32(high_b) + carry
+                
+                return (jnp.int32(low_sum), jnp.int32(high_sum))
+
+            print("tracing post cumsum")
+            if backend == "gpu": 
+                cumsum_low  = jnp.cumsum(nonzero_indices[i]) 
+                cumsum_high = jnp.cumsum(cumsum_low<nonzero_indices[i])
+            else: 
+                cumsum_low  = cumsum_jax(nonzero_indices[i]) 
+                cumsum_high = cumsum_jax(cumsum_low<nonzero_indices[i])
+
+            low, high = add_int64( (cumsum_low, cumsum_high), (offset[0][i], offset[0][i]) )
+
+            if backend == "cpu": 
+                indices = recover(low, high)
+                indices = indices.astype(np.int64)
+                dm_indices, ijkl = cpu_ijkl(indices, symmetry+is_K_matrix*8, indices_func)  
+            else:                
+                dm_indices = ipu_ijkl((low, high), symmetry+is_K_matrix*8, N)  
+
+            dm_values  = jnp.take(dm, dm_indices, axis=0) 
             dm_values = dm_values.at[:].mul( eris ) # this is prod, but re-use variable for inplace update. 
             
-            if backend == "cpu": ss_indices = cpu_ijkl(indices, symmetry+8+is_K_matrix*8, indices_func) 
-            else:                ss_indices = ipu_ijkl(indices, symmetry+8+is_K_matrix*8, N) 
+            if backend == "cpu": 
+                ss_indices, _ = cpu_ijkl(indices, symmetry+8+is_K_matrix*8, indices_func) 
+            else:                
+                ss_indices = ipu_ijkl((low, high), symmetry+8+is_K_matrix*8, N) 
             diff_JK    = diff_JK + jax.ops.segment_sum(dm_values, ss_indices, N**2) * (-HYB_B3LYP/2)**is_K_matrix # causes peak 2 
             
             return diff_JK
 
         batches = nonzero_indices.shape[0] # before pmap, tensor had shape (nipus, batches, -1) so [0]=batches after pmap
+
         diff_JK = jax.lax.fori_loop(0, batches, sequentialized_iter, diff_JK) 
+        #for i in range(batches):
+        #    diff_JK = sequentialized_iter(i, diff_JK)
         return diff_JK
 
     diff_JK = jax.lax.fori_loop(0, 16, iteration, diff_JK) 
-    return jax.lax.psum(diff_JK, axis_name="p")
+    #for i in range(16):
+    #    diff_JK = iteration(i, diff_JK)
+
+    if nipu > 1: return jax.lax.psum(diff_JK, axis_name="p")
+    else: return diff_JK 
 
 if __name__ == "__main__":
     import time 
@@ -129,31 +199,43 @@ if __name__ == "__main__":
     parser.add_argument('-test', action="store_true")
     parser.add_argument('-prof', action="store_true")
     parser.add_argument('-batches', default=5)
-    parser.add_argument('-nipu', default=16)
+    parser.add_argument('-nipu', default=16, type=int)
     parser.add_argument('-skip', action="store_true") 
+    parser.add_argument('-edge', action="store_true") 
     args = parser.parse_args()
     backend = args.backend 
 
     natm = int(args.natm) 
     nipu = int(args.nipu)
-    if backend == "cpu": nipu = 1
+    if backend == "cpu": 
+        nipu = 1
+        jax.config.update('jax_enable_x64', True) 
+    else: 
+        jax.config.update('jax_enable_x64', False) 
 
     start = time.time()
+    #mol = pyscf.gto.Mole(atom="".join(f"C 0 {1.54*j} {1.54*i};" for i in range(natm) for j in range(natm)))
+    # smallest case which overflows in int32! 
+    if args.edge:  mol = pyscf.gto.Mole(atom="".join(f"C 0 {15.4*j} {15.4*i};" for i in range(1) for j in range(65))) 
+    else:          mol = pyscf.gto.Mole(atom="".join(f"C 0 {15.4*j} {15.4*i};" for i in range(1) for j in range(3))) 
 
-    mol = pyscf.gto.Mole(atom="".join(f"C 0 {1.54*j} {1.54*i};" for i in range(natm) for j in range(natm))) 
     mol.build()
     N = mol.nao_nr()
     print("[%i]"%mol.nao_nr())
-    if not args.skip: 
-        print("dense_ERI", time.time()-start)
-        dense_ERI = mol.intor("int2e_sph", aosym="s1")
-    print("distinct_ERI", time.time()-start)
+
+    print("distinct_ERI", time.time()-start, end="")
     distinct_ERI = mol.intor("int2e_sph", aosym="s8")
-    print("Minao init", time.time()-start)
+    print(" ", distinct_ERI.size/10**9, 2**31/10**9) # max int32 for overflow. 
+    if not args.skip: 
+        print("dense_ERI", time.time()-start, end="")
+        dense_ERI = mol.intor("int2e_sph", aosym="s1")
+        print(dense_ERI.shape)
+    print("Minao init", time.time()-start, distinct_ERI.shape)
     dm = pyscf.scf.hf.init_guess_by_minao(mol)         
     scale = HYB_B3LYP/2
     if not args.skip: 
         print("einsum J", time.time()-start)
+        print(dense_ERI.shape, dm.shape)
         J = np.einsum("ijkl,ji->kl", dense_ERI, dm)
         print("einsum K", time.time()-start)
         K = np.einsum("ijkl,jk->il", dense_ERI, dm)
@@ -162,7 +244,10 @@ if __name__ == "__main__":
 
     print("\n----------")
     print("nonzero_indices ", time.time()-start)
-    nonzero_indices      = np.nonzero(distinct_ERI)[0].astype(np.int32)
+    nonzero_indices      = np.nonzero(distinct_ERI)[0]#.astype(np.intu32)
+
+    # could even shift the nonzero_indices[0] to minimize int32 [max-min]
+
     print("grap ERI values", time.time()-start)
     nonzero_distinct_ERI = distinct_ERI[nonzero_indices].astype(np.float32)
     if not args.skip: 
@@ -171,23 +256,6 @@ if __name__ == "__main__":
     print("sparse+symmetric: ", nonzero_distinct_ERI.shape, nonzero_distinct_ERI.nbytes/10**6)
     print("per ipu", nonzero_distinct_ERI.nbytes/10**6/16*2) # obs: most time might be spent compiling loading ERI from H->device! 
     print("----------\n")
-
-    # write code that profiles get_indices/take/segment_sum independently. 
-    # TODO: fix broken test below. add seperate test for get_indices/np.take and segment_sum, potentially with custom poplar implementations
-    if args.prof: 
-        symmetry = 7 
-        dm_indices = get_ijkl(nonzero_indices, symmetry, dm_indices_func_J)  
-
-        size = nonzero_indices.shape[0]
-        total_threads = (1472-1) * 6 
-        remainder = size % total_threads
-        if remainder != 0: 
-            nonzero_indices = np.pad(nonzero_indices, (0, total_threads-remainder))
-        nonzero_indices = nonzero_indices.reshape(total_threads, -1) 
-
-        _dm_indices = np.asarray(_get_ijkl(nonzero_indices, symmetry, N))[:size]
-        assert np.allclose(dm_indices, _dm_indices)
-        exit()
 
     # Compute number of repetitions per nonzero index and re-scale ERI. 
     print("compute eri normalization ", time.time()-start)
@@ -201,22 +269,64 @@ if __name__ == "__main__":
     print(nonzero_distinct_ERI.shape, nonzero_indices.shape)
     batches = int(args.batches) # perhaps make 10 batches? 
     remainder = nonzero_indices.shape[0] % (nipu*batches)
+
+    print("PAD: ", nipu*batches-remainder)
+
+    pad_length = nipu*batches-remainder
     if remainder != 0:
-        nonzero_indices = np.pad(nonzero_indices, (0,nipu*batches-remainder))
-        nonzero_distinct_ERI = np.pad(nonzero_distinct_ERI, (0,nipu*batches-remainder))
+        nonzero_indices      = np.pad(nonzero_indices,      (0, pad_length))
+        nonzero_distinct_ERI = np.pad(nonzero_distinct_ERI, (0, pad_length))
 
     print("reshape to 16 IPUs pmap", time.time()-start)
-    nonzero_indices = nonzero_indices.reshape(nipu, batches, -1)
+    nonzero_indices      = nonzero_indices.reshape(nipu, batches, -1)
     nonzero_distinct_ERI = nonzero_distinct_ERI.reshape(nipu, batches, -1)
+
+    print(nonzero_indices.shape)
+    print(nonzero_distinct_ERI.shape, nonzero_indices[0,:,0].shape)
+    print(nonzero_indices.reshape(-1)[-1], 2**32, nonzero_indices.reshape(-1)[-1]<2**32)
+    a       = np.diff(nonzero_indices, axis=2)#, prepend=nonzero_indices[0,:,0])
+    offset  = nonzero_indices[0, :, 0].reshape(1, -1, 1).astype(np.int64)
+    #nonzero_indices = np.concatenate((offset, a), axis=2)
+    nonzero_indices = np.concatenate((np.zeros(offset.shape), a), axis=2).astype(np.int32)
+    #nonzero_indices = np.concatenate( ( offset, offset+np.cumsum(a, axis=2) ), axis=2)
+
+    # store offset as two int32. 
+    def split(x):
+        low = x.astype(np.int32)
+        high = np.right_shift(low, 31).astype(np.int32)
+        return low, high
+
+    def recover(low, high):
+        return low.astype(np.int64) + jnp.left_shift(high,31).astype(np.int64)
+
+    low, high = split(offset)
+    rec = recover(low, high)
+
+    assert np.allclose(offset, rec)
+    offset = split(offset)
+    #exit()
+    #nonzero_indices = np.diff(nonzero_indices, axis=-1, prepend=nonzero_indices[0,:,0]).astype(np.int32) # compression 
+    #nonzero_indices = np.cumsum(nonzero_indices, axis=-1).astype(np.int64)
+    #print(nonzero_indices.shape)
     print(nonzero_indices.shape)
     print(nonzero_distinct_ERI.shape)
 
     print("call pmap", time.time()-start)
     print("[%i]"%mol.nao_nr())
-    diff_JK = jax.pmap( sparse_symmetric_einsum, in_axes=(0,0,None,None, None), static_broadcasted_argnums=(3,), backend=backend, axis_name="p") (nonzero_distinct_ERI, nonzero_indices, dm, args.backend) 
+    if nipu > 1: 
+        diff_JK = jax.pmap( sparse_symmetric_einsum, in_axes=(0,0,(0,0),None,None, None), static_broadcasted_argnums=(4,5), backend=backend, axis_name="p") (nonzero_distinct_ERI, nonzero_indices, offset, dm, args.backend, args.nipu) 
+    else: 
+        offset  = [o[0] for o in offset]
+        diff_JK = jax.jit(sparse_symmetric_einsum, static_argnums=(4,5,6), backend=backend)(nonzero_distinct_ERI[0], nonzero_indices[0], offset, dm, args.backend, args.nipu, pad_length) 
+        diff_JK = np.asarray(diff_JK)
+        #diff_JK =  sparse_symmetric_einsum(nonzero_distinct_ERI[0], nonzero_indices[0], offset, dm, args.backend, args.nipu) 
+        #diff_JK =  sparse_symmetric_einsum(nonzero_distinct_ERI[0], nonzero_indices[0], offset, dm, args.backend, args.nipu) 
+
     if args.skip: 
         exit()
-    diff_JK = np.array(diff_JK[0])
+
+    if nipu > 1: 
+        diff_JK = np.array(diff_JK[0])
 
     diff_JK = diff_JK.reshape(N, N)
     print(diff_JK.reshape(-1)[::51])
