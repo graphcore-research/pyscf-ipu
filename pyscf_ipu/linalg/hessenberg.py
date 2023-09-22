93a826ad727b91ad1a106d609766f366dd9e54b9
alexandermath <alexm@graphcore.ai>
2023-09-22 10:32:55 +0000

    Rewrote with masking trick to allow jax.lax.fori_loop, now compiles in ~20s for d=1024 and takes ~76M cyles. Best case is ~1M cycles.

--- pyscf_ipu/linalg/hessenberg.py
+++ pyscf_ipu/linalg/hessenberg.py
@@ -11,7 +11,8 @@ def cpu_our_hessenberg(A): # just change three lines of QR
     Q = jnp.identity(A.shape[0])
     R = jnp.copy(A)
 
-    for cnt in range(1, A.shape[0] - 1):  # CHANGE: range(0, ..) to range(1, ..)
+    from tqdm import tqdm 
+    for cnt in tqdm(range(1, A.shape[0] - 1)):  # CHANGE: range(0, ..) to range(1, ..)
         mask = jnp.zeros(A.shape[0])
         mask = mask.at[cnt:].set(1)
         x   = R[:, cnt-1] * mask          # CHANGE: cnt to cnt-1
@@ -33,22 +34,32 @@ def ipu_our_hessenberg(A): # just change three lines of QR
 
     def body(cnt, vals):
         A, R, Q = vals 
-        mask = jnp.zeros(A.shape[0])
-        mask = mask.at[cnt:].set(1)
+        d = A.shape[0]
+        mask = jnp.arange(d)
+        #mask = mask.at[cnt:].set(1)
+        #mask = jax.lax.dynamic_update_slice(mask, jnp.ones((d-cnt)), cnt)
+        #mask = jax.lax.where( jnp.arange(d) )
+        mask = mask >= cnt 
         x   = R[:, cnt-1] * mask          # CHANGE: cnt to cnt-1
 
-        u = x + jnp.concatenate([jnp.zeros(cnt), jnp.array( jnp.copysign(jnp.linalg.norm(x), -A[cnt, cnt]) 
-                                                           ).reshape(1), jnp.zeros(R.shape[0] - 1-cnt) ])
+        #u = x + jnp.concatenate([jnp.zeros(cnt), jnp.array( jnp.copysign(jnp.linalg.norm(x), -A[cnt, cnt]) ).reshape(1), 
+        #                            jnp.zeros(R.shape[0] - 1-cnt) ])
+
+        z = jnp.zeros(x.shape)
+        z = z.at[cnt].set( jnp.copysign(jnp.linalg.norm(x), -A[cnt, cnt]) )
+        u = x + z
+
         v = u / jnp.linalg.norm(u)
 
         # Householder matrix
         R = R - 2 *  v.reshape(-1, 1) @ (v.reshape(1, -1)  @ R)
-        R = R - 2 *  (R @ v.reshape(-1, 1)) @ v.reshape(1, -1)  # CHANGE: add householder mult from right;;
+        R = R - 2 * (R @ v.reshape(-1, 1)) @ v.reshape(1, -1)  # CHANGE: add householder mult from right;;
         Q = Q - 2 * (Q @ v.reshape(-1, 1)) @ v.reshape(1, -1)
 
         return [A, R, Q]
 
-    unroll = True
+
+    unroll = False
     if unroll: 
         for cnt in range(1, A.shape[0] - 1):  # CHANGE: range(0, ..) to range(1, ..)
             A, R, Q = body(cnt, [A, R, Q])
@@ -64,21 +75,23 @@ np.random.seed(42)
 A = np.random.normal(0,1,(d,d))
 A = (A + A.T)/2
 
-Q, H = cpu_our_hessenberg(A)
 _H, _Q = scipy.linalg.hessenberg(A, calc_q=True)
-print("> hessenberg Q")
-pprint(Q[:4, :4])
-pprint(_Q[:4, :4])
-print(np.max(np.abs(np.abs(Q)-np.abs(_Q))))
 
-print("> hessenberg H")
-pprint(H)
-pprint(_H)
-print(np.max(np.abs(np.abs(H)-np.abs(_H))))
+if d < 128: 
+    Q, H = cpu_our_hessenberg(A)
+    print("> hessenberg Q")
+    pprint(Q[:4, :4])
+    pprint(_Q[:4, :4])
+    print(np.max(np.abs(np.abs(Q)-np.abs(_Q))))
+
+    print("> hessenberg H")
+    pprint(H)
+    pprint(_H)
+    print(np.max(np.abs(np.abs(H)-np.abs(_H))))
 
 
-assert np.allclose(np.abs(Q), np.abs(_Q), atol=1e-6)
-assert np.allclose(np.abs(H), np.abs(_H), atol=1e-6)
+    assert np.allclose(np.abs(Q), np.abs(_Q), atol=1e-6)
+    assert np.allclose(np.abs(H), np.abs(_H), atol=1e-6)
 
 jax.config.update('jax_enable_x64', False)
 Q, H = jax.jit(ipu_our_hessenberg, backend="ipu")(A)
