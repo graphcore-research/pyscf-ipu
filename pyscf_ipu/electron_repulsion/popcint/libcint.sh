46a26f755054d1c9ef4bf45652a4e576997d9aba
alexandermath <alexm@graphcore.ai>
2023-10-01 09:14:10 +0000

    Fixed bug in script to run test cases.

--- pyscf_ipu/electron_repulsion/popcint/libcint.sh
+++ pyscf_ipu/electron_repulsion/popcint/libcint.sh
@@ -1,7 +1,7 @@
 clear
 rm libcint.so 
 
-cc _libcint.c -shared -fpic -o libcint.so -lpoplar -lpoputil -fpermissive
+g++ libcint.c -shared -fpic -o libcint.so -lpoplar -lpoputil -fpermissive
 echo "Done compiling. Calling C code from python. "
 
-XLA_IPU_PLATFORM_DEVICE_COUNT=1 TF_POPLAR_FLAGS=--show_progress_bar=true python libcint.py
+XLA_IPU_PLATFORM_DEVICE_COUNT=1 TF_POPLAR_FLAGS=--show_progress_bar=true python libcint.py $@
