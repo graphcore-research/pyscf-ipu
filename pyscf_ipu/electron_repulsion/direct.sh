20f62eaa5c0e946513315f56d40e32347ebfa73d
alexandermath <alexm@graphcore.ai>
2023-10-01 08:46:51 +0000

    adding single ipu profiling to direct.sh script.

--- pyscf_ipu/electron_repulsion/direct.sh
+++ pyscf_ipu/electron_repulsion/direct.sh
@@ -5,4 +5,7 @@ cc cpu_int2e_sph.cpp -shared -fpic -o gen.so  -lpoplar -lpoputil -fpermissive
 echo "Done compiling"
 #echo "Calling from python";
 
-TF_POPLAR_FLAGS=--show_progress_bar=true python direct.py $@
+XLA_IPU_PLATFORM_DEVICE_COUNT=1 POPLAR_ENGINE_OPTIONS="{
+\"autoReport.outputExecutionProfile\": \"true\",
+\"autoReport.directory\": \"profs/\"
+}" TF_POPLAR_FLAGS=--show_progress_bar=true python direct.py $@
