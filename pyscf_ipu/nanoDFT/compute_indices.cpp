6a191309e24e2a5d9e5a4a96bc8d46537b716796
alexandermath <alexm@graphcore.ai>
2023-10-10 09:20:19 +0000

    Work on fixing tile memory layout for einsum.

--- pyscf_ipu/nanoDFT/compute_indices.cpp
+++ pyscf_ipu/nanoDFT/compute_indices.cpp
@@ -21,7 +21,7 @@ using namespace poplar;
 
 class IndicesIJKL : public Vertex {
 public:
-  Input<Vector<const int>> outshape;
+  Input<Vector<const uint32_t>> outshape;
   //Input<Vector<const int>> j_;
   //Input<Vector<const int>> k_;
   //Input<Vector<const int>> l_;
@@ -33,7 +33,7 @@ public:
   Input<Vector<const uint32_t>> start_;
   Input<Vector<const uint32_t>> stop_;
 
-  Output<Vector<int>> out_; 
+  Output<Vector<uint32_t>> out_; 
 
   bool compute() {
 
@@ -53,10 +53,11 @@ public:
       const int& k = nonzero_indices[iteration*4+2];
       const int& l = nonzero_indices[iteration*4+3];
 
-      int& out = out_[iteration];
+      uint32_t& out = out_[iteration];
 
       //_compute_symmetry(ij, kl, N, sym, out[iteration]);
       switch (sym) {
+        // dm 
         case 0:  { out = i*N+j; break; }
         case 1:  { out = j*N+i; break; }
         case 2:  { out = i*N+j; break; }
@@ -104,3 +105,122 @@ public:
     return true;
   }
 };
+
+
+
+template <int SYM> 
+class TemplatedIndicesIJKL : public Vertex {
+public:
+  Input<Vector<const uint32_t>> outshape;
+  Input<Vector<const int>> nonzero_indices;
+  //Input<Vector<const uint32_t>> sym_;
+  Input<Vector<const uint32_t>> N_;
+  Input<Vector<const uint32_t>> start_;
+  Input<Vector<const uint32_t>> stop_;
+
+  Output<Vector<uint32_t>> out_; 
+
+  bool compute() {
+
+    const uint32_t& N     = N_[0];
+    //const uint32_t& sym   = sym_[0];
+    const uint32_t& start = start_[0];
+    const uint32_t& stop  = stop_[0];
+
+    for (uint32_t iteration = start; iteration < stop; iteration++){
+
+      //const int& i = i_[iteration]; 
+      //const int& j = j_[iteration];
+      //const int& k = k_[iteration];
+      //const int& l = l_[iteration];
+      const int& i = nonzero_indices[iteration*4+0];
+      const int& j = nonzero_indices[iteration*4+1];
+      const int& k = nonzero_indices[iteration*4+2];
+      const int& l = nonzero_indices[iteration*4+3];
+
+      uint32_t& out = out_[iteration];
+
+      //_compute_symmetry(ij, kl, N, sym, out[iteration]);
+      //switch (SYM ) {
+        if constexpr (SYM == 0)  { out = i*N+j; }
+        else if constexpr (SYM == 1)  { out = j*N+i; }
+        else if constexpr (SYM == 2)  { out = i*N+j; }
+        else if constexpr (SYM == 3)  { out = j*N+i; }
+        else if constexpr (SYM == 4)  { out = k*N+l; }
+        else if constexpr (SYM == 5)  { out = l*N+k; }
+        else if constexpr (SYM == 6)  { out = k*N+l; }
+        else if constexpr (SYM == 7)  { out = l*N+k; }
+
+
+        //ss_indices_func_J = lambda i,j,k,l,symmetry: jnp.array([k*N+l, k*N+l, l*N+k, l*N+k, i*N+j, i*N+j, j*N+i, j*N+i])[symmetry]
+        else if constexpr (SYM == 8)  { out = k*N+l; }
+        else if constexpr (SYM == 9)  { out = k*N+l; }
+        else if constexpr (SYM == 10) { out = l*N+k; }
+        else if constexpr (SYM == 11) { out = l*N+k; }
+        else if constexpr (SYM == 12) { out = i*N+j; }
+        else if constexpr (SYM == 13) { out = i*N+j; }
+        else if constexpr (SYM == 14) { out = j*N+i; }
+        else if constexpr (SYM == 15) { out = j*N+i; }
+
+
+        //dm_indices_func_K = lambda i,j,k,l,symmetry: jnp.array([k*N+j, k*N+i, l*N+j, l*N+i, i*N+l, i*N+k, j*N+l, j*N+k])[symmetry]
+        else if constexpr (SYM == 16) { out = k*N+j; }
+        else if constexpr (SYM == 17) { out = k*N+i; }
+        else if constexpr (SYM == 18) { out = l*N+j; }
+        else if constexpr (SYM == 19) { out = l*N+i; }
+        else if constexpr (SYM == 20) { out = i*N+l; }
+        else if constexpr (SYM == 21) { out = i*N+k; }
+        else if constexpr (SYM == 22) { out = j*N+l; }
+        else if constexpr (SYM == 23) { out = j*N+k; }
+
+
+        //ss_indices_func_K = lambda i,j,k,l,symmetry: jnp.array([i*N+l, j*N+l, i*N+k, j*N+k, k*N+j, l*N+j, k*N+i, l*N+i])[symmetry]
+        else if constexpr (SYM == 24) { out = i*N+l; }
+        else if constexpr (SYM == 25) { out = j*N+l; }
+        else if constexpr (SYM == 26) { out = i*N+k; }
+        else if constexpr (SYM == 27) { out = j*N+k; }
+        else if constexpr (SYM == 28) { out = k*N+j; }
+        else if constexpr (SYM == 29) { out = l*N+j; }
+        else if constexpr (SYM == 30) { out = k*N+i; }
+        else if constexpr (SYM == 31) { out = l*N+i; }
+      //}
+
+    }
+    return true;
+  }
+};
+
+
+// explicit instantiations
+template class TemplatedIndicesIJKL<0>;
+template class TemplatedIndicesIJKL<1>;
+template class TemplatedIndicesIJKL<2>;
+template class TemplatedIndicesIJKL<3>;
+template class TemplatedIndicesIJKL<4>;
+template class TemplatedIndicesIJKL<5>;
+template class TemplatedIndicesIJKL<6>;
+template class TemplatedIndicesIJKL<7>;
+template class TemplatedIndicesIJKL<8>;
+template class TemplatedIndicesIJKL<9>;
+template class TemplatedIndicesIJKL<10>;
+template class TemplatedIndicesIJKL<11>;
+template class TemplatedIndicesIJKL<12>;
+template class TemplatedIndicesIJKL<13>;
+template class TemplatedIndicesIJKL<14>;
+template class TemplatedIndicesIJKL<15>;
+template class TemplatedIndicesIJKL<16>;
+template class TemplatedIndicesIJKL<17>;
+template class TemplatedIndicesIJKL<18>;
+template class TemplatedIndicesIJKL<19>;
+template class TemplatedIndicesIJKL<20>;
+template class TemplatedIndicesIJKL<21>;
+template class TemplatedIndicesIJKL<22>;
+template class TemplatedIndicesIJKL<23>;
+template class TemplatedIndicesIJKL<24>;
+template class TemplatedIndicesIJKL<25>;
+template class TemplatedIndicesIJKL<26>;
+template class TemplatedIndicesIJKL<27>;
+template class TemplatedIndicesIJKL<28>;
+template class TemplatedIndicesIJKL<29>;
+template class TemplatedIndicesIJKL<30>;
+template class TemplatedIndicesIJKL<31>;
--- pyscf_ipu/nanoDFT/sparse_symmetric_ERI.py
+++ pyscf_ipu/nanoDFT/sparse_symmetric_ERI.py
@@ -20,22 +20,20 @@ def cpu_ijkl(value, symmetry, N, f):
     return f(i,j,k,l,symmetry,N)
 cpu_ijkl = jax.vmap(cpu_ijkl, in_axes=(0, None, None, None))
 
-
-
 from tessellate_ipu import create_ipu_tile_primitive, ipu_cycle_count, tile_map, tile_put_sharded, tile_put_replicated
 vertex_filename  = osp.join(osp.dirname(__file__), "compute_indices.cpp")
-compute_indices= create_ipu_tile_primitive(
-        "IndicesIJKL" ,
-        "IndicesIJKL" ,
-        #inputs=["i_", "j_", "k_", "l_", "nonzero_indices", "sym_", "N_", "start_", "stop_"], 
-        inputs=["outshape", "nonzero_indices", "sym_", "N_", "start_", "stop_"], 
+compute_indices= [create_ipu_tile_primitive(
+        "TemplatedIndicesIJKL<%i>"%a ,
+        "TemplatedIndicesIJKL<%i>"%a ,
+        inputs=["outshape", "nonzero_indices", "N_", "start_", "stop_"], 
         outputs={"out_": 0},
         gp_filename=vertex_filename,
         perf_estimate=100,
-)
+) for a in range(32)]
 
 @partial(jax.jit, backend="ipu")
 def ipu_ijkl(nonzero_indices, symmetry, N):
+    nonzero_indices = nonzero_indices[:, 0]
     size          = nonzero_indices.shape[0]
     total_threads = (1472-1) * 6 
     remainder     = size % total_threads
@@ -78,61 +76,127 @@ indices_func = lambda i,j,k,l,symmetry,N: jnp.array([i*N+j, j*N+i, i*N+j, j*N+i,
                                                      i*N+l, j*N+l, i*N+k, j*N+k, k*N+j, l*N+j, k*N+i, l*N+i])[symmetry]
 
 def sparse_symmetric_einsum(nonzero_distinct_ERI, nonzero_indices, dm, backend):
-    dm = dm.reshape(-1)
+    dm      = dm.reshape(-1)
     diff_JK = jnp.zeros(dm.shape)
-    N = int(np.sqrt(dm.shape[0]))
 
-    dnums = jax.lax.GatherDimensionNumbers(
-        offset_dims=(), 
-        collapsed_slice_dims=(0,),
-        start_index_map=(0,))
-    scatter_dnums = jax.lax.ScatterDimensionNumbers(
-    update_window_dims=(), 
-    inserted_window_dims=(0,),
-    scatter_dims_to_operand_dims=(0,))
+    N       = int(np.sqrt(dm.shape[0]))
 
-    def iteration(symmetry, vals): 
-        diff_JK = vals 
-        is_K_matrix =  (symmetry >= 8)
+    # Arguments for gather/scatter_add. 
+    dnums         = jax.lax.GatherDimensionNumbers( offset_dims=(), collapsed_slice_dims=(0,), start_index_map=(0,))
+    scatter_dnums = jax.lax.ScatterDimensionNumbers( update_window_dims=(), inserted_window_dims=(0,), scatter_dims_to_operand_dims=(0,))
 
-        def sequentialized_iter(i, vals):
-            # Generalized J/K computation: does J when symmetry is in range(0,8) and K when symmetry is in range(8,16)
-            # Trade-off: Using one function leads to smaller always-live memory.
-            diff_JK = vals 
+    # Initialize memory layout.
+    SKIP_TILES = 1 
+    NUM_TILES  = 1471
+    num        = nonzero_distinct_ERI.shape[1] 
+    tiles      = tuple((np.arange(0,num) % (NUM_TILES) + SKIP_TILES).astype(np.uint32).tolist())
+    eris       = tile_put_sharded(nonzero_distinct_ERI.T, tiles=tiles)
 
-            indices = nonzero_indices[i]
-            print(nonzero_indices.shape, indices.shape)
+    nonzero_indices = nonzero_indices.reshape(batches, num, 1, 4)
+    nonzero_indices = tile_put_sharded(jnp.transpose(nonzero_indices, (1, 0, 2, 3)), tiles=tiles)
 
-            # move the casting from f16 to int32 to inside c code. 
-            indices = jax.lax.bitcast_convert_type(indices, np.int16).astype(np.int32)
-            eris    = nonzero_distinct_ERI[i]
+    stop       = nonzero_indices.shape[2]
+    outshape   = np.zeros((num, stop, 1), dtype=np.uint32) # perhaps faster if jnp? 
 
-            if backend == "cpu": dm_indices = cpu_ijkl(indices, symmetry+is_K_matrix*8, N, indices_func).reshape(-1, 1)
-            else:                dm_indices = ipu_ijkl(indices, symmetry+is_K_matrix*8, N)  .reshape(-1, 1)
-            # dm_values = jnp.take(dm, indices, axis=0) # for our special case the 50 lines of code reduces to the one line below. 
-            print(dm.shape, dm_indices.shape)
-            exit()
-            dm_values = jax.lax.gather(dm, dm_indices, dimension_numbers=dnums, slice_sizes=(1,), mode=jax.lax.GatherScatterMode.FILL_OR_DROP)
+    multiplier = -HYB_B3LYP/2
+    mult       = jnp.ones(N**2)*multiplier
+    mult       = tile_put_replicated(mult, tiles=tiles)
 
-            dm_values = dm_values.at[:].mul( eris ) # this is prod, but re-use variable for inplace update. 
+    # dm is symmetric, only take the distinct entris 
+    #dm = dm[:N*(N-1)//2]  
+    #dm = dm[:N*N)//2]  
+
+    dm_scaled  = tile_put_replicated(dm*multiplier, tiles)
+    #dm_scaled  = dm*multiplier
+    #dm         = tile_put_replicated(dm, tiles)
+    print(dm.shape)
+    dm = dm.reshape(2, 450)
+    diff_JK    = tile_put_replicated(diff_JK, tiles=tiles)
+
+    N         = tile_put_replicated(jnp.array(N,        dtype=jnp.uint32),   tiles)
+    start     = tile_put_replicated(jnp.array(0,        dtype=jnp.uint32),   tiles)
+    stop      = tile_put_replicated(jnp.array(stop,     dtype=jnp.uint32),   tiles)
+    outshape  = tile_put_sharded(outshape, tiles)
+
+    def iteration(i, diff_JK): 
+        def sequentialized_iter(symmetry, diff_JK, dm, dm_scaled, is_K_matrix=False):
+            # Step 1: dm_indices = ipu_ijkl(indices, symmetry+is_K_matrix*8, N).reshape(-1, 1)
+            nonzero_indices_i = nonzero_indices[:, i] 
+            dm_indices        = tile_map(compute_indices[symmetry + is_K_matrix*8], outshape, nonzero_indices_i, N, start, stop)
+            dm_indices = tile_put_sharded(dm_indices.array-first_half*step, tiles)
+
+            # Step 2: dm_values = jnp.take(dm, indices, axis=0) 
+            dm_values = tile_map( jax.lax.gather_p,
+                                    dm_scaled if is_K_matrix else dm, # faster with pre-scaled dm 
+                                    dm_indices,
+                                    dimension_numbers=dnums, slice_sizes=(1,), mode=jax.lax.GatherScatterMode.PROMISE_IN_BOUNDS,
+                                    unique_indices=False, indices_are_sorted=False, fill_value=None,
+            )
+
+            # Step 3: prod 
+            dm_values = tile_map(jax.lax.mul_p, dm_values, eris[:, i])
             
-            if backend == "cpu": ss_indices = cpu_ijkl(indices, symmetry+8+is_K_matrix*8, N, indices_func) .reshape(-1,1)
-            else:                ss_indices = ipu_ijkl(indices, symmetry+8+is_K_matrix*8, N).astype(np.int32).reshape(-1,1)
-            # diff_JK = diff_JK + jax.lax.segment_sum( ...) # for our special case the 100 lines of code reduces to the one line below. 
-            diff_JK = diff_JK + jax.lax.scatter_add(jnp.zeros((N**2,)),
-                                            ss_indices, dm_values, 
-                                            scatter_dnums, indices_are_sorted=True, unique_indices=True, mode=jax.lax.GatherScatterMode.FILL_OR_DROP)\
-                                *(-HYB_B3LYP/2)**is_K_matrix
+            # Step 4: ss_indices = ipu_ijkl(nonzero_indices[i], symmetry+8+is_K_matrix*8, N).reshape(-1,1)
+            ss_indices = tile_map(compute_indices[symmetry+8+is_K_matrix*8], outshape, nonzero_indices_i, N, start, stop)
             
-            return diff_JK
+            # Step 5: diff_JK = jax.lax.segment_sum(diff_JK, ss_indices, dm_values, ...)
+            return tile_map(
+                jax.lax.scatter_add_p,
+                diff_JK,
+                ss_indices,
+                dm_values,
+                dimension_numbers=scatter_dnums,
+                indices_are_sorted=False,
+                unique_indices=False,
+                mode=jax.lax.GatherScatterMode.PROMISE_IN_BOUNDS,
+                update_jaxpr=None,
+                update_consts=None,
+            )
 
-        batches = nonzero_indices.shape[0] # Before pmap, tensor had shape (nipus, batches, -1) so [0]=batches after pmap
         #diff_JK = jax.lax.fori_loop(0, batches, sequentialized_iter, diff_JK) 
-        diff_JK = jax.lax.fori_loop(0, batches, sequentialized_iter, diff_JK) 
+        sequentialized_iter_J = lambda x,y,z,a: sequentialized_iter(x,y,z,a,False) 
+        sequentialized_iter_K = lambda x,y,z,a: sequentialized_iter(x,y,z,a,True) 
+        #diff_JK = jax.lax.fori_loop(0, 8, sequentialized_iter_J, diff_JK) 
+        #diff_JK = jax.lax.fori_loop(8, 16, sequentialized_iter_K, diff_JK) 
+
+        #tile_shard_dm 
+        # // may add a 10x here. 
+
+        first_half = i < (batches//2)
+        step = dm.shape[0]//2
+        print(first_half, step)
+
+        _dm_scaled = tile_put_replicated(dm[first_half]*multiplier, tiles)
+        _dm        = tile_put_replicated(dm[first_half], tiles)
+        #diff_JK   = tile_put_replicated(diff_JK, tiles=tiles)
+
+        # this basically uses the same floats 16 times; 
+        # we don't want to reload stuff for that, i.e., have them close to each other. 
+        for z in range(8):
+            diff_JK = sequentialized_iter_J(z, diff_JK, _dm, _dm_scaled)
+        for z in range(8, 16):
+            diff_JK = sequentialized_iter_K(z, diff_JK, _dm, _dm_scaled) 
+
+        #dm_scaled = jnp.sum(dm_scaled.array, axis=0)
+        #dm = dm.array
+
+        #for i in range(batches):
+        #    diff_JK = sequentialized_iter(i, diff_JK)
+        #tile-reduce_sum here # |batch_size| more reduce_sums.  only of size N^2/|batch_size. 
+        #  => only adds N^2 reduce_sum. 
+
         return diff_JK
 
     #diff_JK = jax.lax.fori_loop(0, 16, iteration, diff_JK) 
-    diff_JK = jax.lax.fori_loop(0, 16, iteration, diff_JK) 
+    #for i in range(16):
+    #    diff_JK = iteration(i, diff_JK)
+    diff_JK = jax.lax.fori_loop(0, batches, iteration, diff_JK) 
+    #for i in range(batches):
+    #    diff_JK = iteration(i, diff_JK)
+    #return jax.lax.psum(diff_JK, axis_name="p")
+    print(diff_JK.shape)
+    diff_JK = diff_JK.array.sum(0)
+    print(diff_JK.shape)
     return jax.lax.psum(diff_JK, axis_name="p")
 
 if __name__ == "__main__":
@@ -156,7 +220,8 @@ if __name__ == "__main__":
 
     start = time.time()
 
-    mol = pyscf.gto.Mole(atom="".join(f"C 0 {1.54*j} {1.54*i};" for i in range(natm) for j in range(natm))) 
+    #mol = pyscf.gto.Mole(atom="".join(f"C 0 {1.54*j} {1.54*i};" for i in range(natm) for j in range(natm))) 
+    mol = pyscf.gto.Mole(atom="".join(f"C 0 {1.54*j} {1.54*i};" for i in range(2) for j in range(3))) 
     #mol = pyscf.gto.Mole(atom="".join(f"C 0 {15.4*j} {15.4*i};" for i in range(1) for j in range(75))) 
     mol.build()
     N = mol.nao_nr()
@@ -177,12 +242,21 @@ if __name__ == "__main__":
     nonzero_distinct_ERI = distinct_ERI[nonzero_indices].astype(np.float32)
     print("Nonzero Operations:", nonzero_indices.size*8*2/10**9, "[Giga]")
     ij, kl               = get_i_j(nonzero_indices)
+    print(ij)
+    # does transpose just correspond to exchange ij, kl? 
+    # i want to exchange the order we visit the elements in, i.e., visit column by column. 
+
+    # comput batch_size so we get as close as possible to  NUM_TILES*THREADS 
     rep                  = num_repetitions_fast(ij, kl)
     nonzero_distinct_ERI = nonzero_distinct_ERI / rep
     dm                   = dm.reshape(-1)
     diff_JK              = np.zeros(dm.shape)
-
     batches  = int(args.batches) # perhaps make 10 batches? 
+
+
+    NUM_TILES = 1471 
+    batches = int(nonzero_indices.shape[0]//(NUM_TILES*6))+1
+
     remainder = nonzero_indices.shape[0] % (nipu*batches)
 
     if remainder != 0:
@@ -191,16 +265,68 @@ if __name__ == "__main__":
         kl = np.pad(kl, ((0,nipu*batches-remainder)))
         nonzero_distinct_ERI = np.pad(nonzero_distinct_ERI, (0,nipu*batches-remainder))
 
+    # Have column on single tile 
+    # split so each tile gets a column, i.e., only needs one element from dm. 
+    
+    # can I arbirarily permute at this point? 
+
+    print(ij.shape, kl.shape, nonzero_distinct_ERI.shape)
+    #indxs = np.argsort(np.random.normal(0, 1, ij.shape[0]))
+    indxs = np.argsort(kl)
+    ij, kl, nonzero_distinct_ERI = ij[indxs], kl[indxs], nonzero_distinct_ERI[indxs]
+
+    # the batching isn't perfectly uniform. 
+    # for some reason, the matrix has more elemnts in earlier columns than later. 
+    # perhaps this is due to all of the symmetry? 
+    # this means basically we will touch th first entries of DM much more than the later entries of dm .
+    # oh, but if we also utilize the symmetry in dm matrix I think this may even out! 
+    # so might be perfectly balanced! 
+    print(kl)
+
+    import matplotlib.pyplot as plt 
+    fig, ax = plt.subplots(1,2)
+    ax[0].plot(kl)
+    # we are indexing into entry (ij, kl); 
+    # i want to sort so we do 
+
+    # so dm is symmetric => only need to store half of it! 
+    # dm = dm.reshape(-1)[:N*(N-1)//2]
+
     ij = ij.reshape(nipu, batches, -1)
     kl = kl.reshape(nipu, batches, -1)
+    print(batches)
+    for i in range(batches):
+        print(kl[0,i].min(), kl[0, i].max())
+        ax[1].plot(kl[0,i])
+    plt.savefig("cols.jpg")
+
+
     nonzero_distinct_ERI = nonzero_distinct_ERI.reshape(nipu, batches, -1)
 
     i, j = get_i_j(ij.reshape(-1))
     k, l  = get_i_j(kl.reshape(-1))
-    nonzero_indices = np.vstack([i,j,k,l]).T.reshape(nipu, batches, -1, 4).astype(np.int16)
-    nonzero_indices = jax.lax.bitcast_convert_type(nonzero_indices, np.float16)
+    nonzero_indices = np.vstack([i,j,k,l]).T.reshape(nipu, batches, -1, 4).astype(np.int32)
+
+    np.random.seed(42)
+    # we can permute arbitraily! 
+    #indxs = np.argsort(np.random.normal(0, 1, nonzero_indices.shape[2]))
+    #indxs = np.argsort(np.abs(nonzero_distinct_ERI[0,0]))
+    #indxs = np.argsort(np.abs(nonzero_indices[0,0]))
+    indxs = np.arange(nonzero_indices.shape[2])
+
+    print(nonzero_indices[0,0, :5])
+    nonzero_indices = nonzero_indices[:, :, indxs]
+    print(nonzero_indices[0,0, :5])
+    print("--> %i : %i <--"%(batches, nonzero_indices.shape[2]))
+
+    #nonzero_indices = jax.lax.bitcast_convert_type(nonzero_indices, np.float16)
+    print(nonzero_distinct_ERI[0,0,:5])
+    nonzero_distinct_ERI = nonzero_distinct_ERI[:, :, indxs] 
+    print(nonzero_distinct_ERI[0,0,:5])
 
     diff_JK = jax.pmap(sparse_symmetric_einsum, in_axes=(0,0,None,None), static_broadcasted_argnums=(3,), backend=backend, axis_name="p")(nonzero_distinct_ERI, nonzero_indices, dm, args.backend) 
+    #diff_JK = jax.pmap(sparse_symmetric_einsum, in_axes=(0,0,None,None), static_broadcasted_argnums=(3,), backend=backend, axis_name="p")(nonzero_distinct_ERI, nonzero_indices, dm, args.backend) 
+    #diff_JK = jax.jit(sparse_symmetric_einsum(nonzero_distinct_ERI[0], nonzero_indices[0], dm, args.backend) 
 
     if args.skip: 
         exit()
@@ -209,7 +335,28 @@ if __name__ == "__main__":
 
     diff_JK = diff_JK.reshape(N, N)
     print(diff_JK.reshape(-1)[::51])
-    print(truth.reshape(-1)[::51])
+    print(truth.reshape(-1)[::51])  
+    print(diff_JK.shape, truth.shape)
     print(np.max(np.abs(diff_JK.reshape(-1) - truth.reshape(-1))))
-    assert np.allclose(diff_JK, truth, atol=1e-6)
+
+    # deal with diagonal entries N^2 using double precision? 
+    import matplotlib.pyplot as plt 
+
+    fig, ax= plt.subplots(1,1)
+    diff = np.abs(diff_JK.reshape(-1)-truth.reshape(-1))
+    real_diff = diff
+    indxs = np.argsort(diff)
+    diff = np.abs(diff_JK.reshape(-1)-truth.reshape(-1))/np.abs(truth.reshape(-1))
+    diff[np.abs(truth.reshape(-1)) < 1e-9] = 0 
+    #ax.plot(diff[indxs], 'x', ms=2, label="relative error")
+    ax.plot(np.abs(truth.reshape(-1))[indxs], 'o', ms=2, label="real abs value")
+    ax.plot(real_diff[indxs], label="absolute error")
+    plt.legend()
+
+    ax.set_yscale("log")
+
+    plt.savefig("error_jk.jpg")
+    
+
+    assert np.allclose(diff_JK, truth, atol=1e-6) # maybe make image of abs/relative error? do on image of matrix aswell? 
     print("PASSED!")
